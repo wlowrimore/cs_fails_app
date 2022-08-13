9048424113edef1071b76895325aea6dac81bab4
William Lowrimore <wlowrimore@gmail.com>
2022-08-13 10:34:43 -0500

    adds basic landing page with route, and begins creating sign up page.

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def policy
+  end
+
+  def terms
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root "articles#index"
+  root "static_pages#index"
+
+  get "sign_up", to: "registrations#new"
   
   resources :articles do
     resources :comments
