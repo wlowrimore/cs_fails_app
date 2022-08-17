2a0c68dc2dd1e4d04146875cb9e22e598edba782
William Lowrimore <wlowrimore@gmail.com>
2022-08-16 22:12:22 -0500

    adds temporary login page...will restructure

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  get "login", to: "registrations#login"
+  post "login", to: "registrations#login"
   
   resources :articles do
     resources :comments
